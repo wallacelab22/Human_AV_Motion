be2abeb2c2b815d72246af7a6d9f3bdaf83128f1
Adam Tiesman <adam.j.tiesman@vanderbilt.edu>
2023-02-17 11:02:04 -0600

    completed rough draft of plot script

--- Psychometric_Function_Plot/TEST_Human_Visual_Figure.m
+++ Psychometric_Function_Plot/TEST_Human_Visual_Figure.m
@@ -1,6 +1,6 @@
 %% TEST Human Visual Figures %%%%%%%%%%
 % written 02/16/23 - Adam Tiesman
-clear all;
+clear;
 close all;
 sca;
 
@@ -19,41 +19,55 @@ chosen_threshold = 0.72; %Ask Mark about threshold
 % Replace all the 0s to 3s for catch trials for splitapply
 data_output(data_output(:, 1) == 0, 1) = 3; 
 
-%Group trials based on stimulus--> 1 = right, 2 = left, 3 = catch
-% right_or_left = data_output(:, 1);
-% right_vs_left = splitapply(@(x){x}, data_output, right_or_left);
+%Group trials based on stimulus direction--> 1 = right, 2 = left, 3 = catch
+right_or_left = data_output(:, 1);
+right_vs_left = splitapply(@(x){x}, data_output, right_or_left);
 
-% Group the rows by the coherences
-groups = findgroups(data_output(:,2));
+% Isolate coherences for right and left groups
+right_group = findgroups(right_vs_left{1,1}(:,2));
+left_group = findgroups(right_vs_left{2,1}(:,2));
+catch_group = findgroups(right_vs_left{3,1}(:,2));
+
+%Initialize an empty array to store rightward_prob for all coherences
+rightward_prob = [];
 
 % Loop over each coherence level and extract the corresponding rows of the matrix for
-% i = 1:max(groups)
-for i = 1:max(groups)
-    group_rows = data_output(groups == i, :);
-    right_or_left = group_rows(:, 3);
-    right_vs_left = splitapply(@(x){x}, group_rows, right_or_left);
-    disp(right_vs_left)
+% i = max(groups):-1:1 for leftward trials
+for i = max(left_group):-1:1
+    group_rows = right_vs_left{2,1}(left_group == i, :);
+    left_var = 2;
+    logical_array = group_rows(:, 3) == left_var;
+    count = sum(logical_array);
+    percentage = 1 - (count/ size(group_rows, 1));
+    rightward_prob = [rightward_prob percentage];
 end
 
-% Display the groups
-disp(groups_rows)
+% Add to the righward_prob vector the catch trials
+group_rows = right_vs_left{3,1};
+right_var = 1;
+logical_array = group_rows(:, 3) == right_var;
+count = sum(logical_array);
+percentage = (count/ size(group_rows, 1));
+rightward_prob = [rightward_prob percentage];
 
-% Provide coherence levels
-coherences = 0:0.01:1;
+% Loop over each coherence level and extract the corresponding rows of the matrix for
+% i = 1:max(groups) for rightward trials
+for i = 1:max(right_group)
+    group_rows = right_vs_left{1,1}(right_group == i, :);
+    right_var = 1;
+    logical_array = group_rows(:, 3) == right_var;
+    count = sum(logical_array);
+    percentage = count/ size(group_rows, 1);
+    rightward_prob = [rightward_prob percentage];
+end
 
-% Define the psychometric function parameters
-threshold = 0.5;  % coherence at which probability of rightward response is 50%
-slope = 2;        % slope of the psychometric function
-guess = 0.5;      % probability of rightward response for zero coherence
-lapse = 0.05;     % probability of a random incorrect response
+% Display prob of right response at each coherence from -5 to 5 (neg being
+% leftward trials and pos being rightward trials)
+coherence_lvls = [-5, -4, -3, -2, -1, 0, 1, 2, 3 , 4, 5];
+scatter(coherence_lvls, rightward_prob);
 
-% Define the psychometric function
-prob_right = guess + (1 - guess - lapse) * (1 - 1./(1+exp(slope*(threshold-coherences))));
+% Add title and labels to the x and y axis
+xlabel('Coherence Level');
+ylabel('Rightward Response Probability');
+title('TEST Human Visual Psychometric Plot');
 
-% Plot the psychometric function
-plot(coherences, prob_right, 'LineWidth', 2);
-
-% Set axis labels and plot title
-xlabel('Coherence');
-ylabel('Probability of rightward response');
-title('Psychometric Function');
