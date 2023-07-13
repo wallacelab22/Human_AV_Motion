09e4a76625af2eec008fa78489b7a57507b95c46
Adam Tiesman <adam.j.tiesman@vanderbilt.edu>
2023-07-13 11:58:45 -0500

    code cleaning update

--- AAA_RDKHoop_stairVis.m
+++ AAA_RDKHoop_stairVis.m
@@ -1,9 +1,10 @@
 %% VISUAL STAIRCASE %%%%%%%%%%
-% written by Adam Tiesman 2/27/2023
+% written by Adam Tiesman - adam.j.tiesman@vanderbilt.edu
+% Initial commit on 2/27/2023
 clear;
 close all;
 
-%% FOR RESPONSE CODING: 1= RIGHTWARD MOTION ; 2=LEFTWARD MOTION
+%% FOR RESPONSE CODING: 1= RIGHTWARD MOTION; 2=LEFTWARD MOTION
 
 % Variables created to navigate code folders throughout script.
 scriptdirectory = '/home/wallace/Human_AV_Motion';
@@ -19,6 +20,8 @@ KbName('UnifyKeyNames');
 AssertOpenGL;
 
 %% define general values
+% minNum, maxNum, and meanNum all deal with the intertrial interval, which
+% is generated in the function iti_response_recording.
 inputtype=1; typeInt=1; minNum=1.5; maxNum=2.5; meanNum=2;
 
 % Specify if you want data analysis. If 1, task code will print out figures
@@ -28,23 +31,27 @@ data_analysis = input('Data Analysis? 0 = NO, 1 = YES : ');
 % Specify the dot speed in the RDK, wil be an input to function
 % at_createDotInfo
 block_dot_speed = input('Dot Speed (in deg/sec): ');
-vel_stair = 0;
-vel_index = 0;
 visInfo.vel = block_dot_speed;
 
+% Set these parameters to 0 so staircase_procedure function knows not to
+% manipulate velocity.
+vel_stair = 0;
+vel_index = 0;
+
 %% General stimlus variables
-% dur is stimulus duration, triallength is total length of 1 trial.
-% nbblocks is used to divide up num_trials into equal parts to give subject
-% breaks if there are many trials. Set to 0 if num_trials is short and
-% subject does not need breaks
+% dur is stimulus duration, triallength is total length of 1 trial (this is
+% currently unused in code), nbblocks is used to divide up num_trials 
+% into equal parts to give subject breaks if there are many trials. 
+% Set to 0 if num_trials is short and subject does not need break(s).
 dur= 0.5; triallength=2; nbblocks=2;
 
-% Define Stimulus repetitions
+% Define stimulus repetitions
 num_trials = 100;
 
-% Visual stimulus properties relating to monitor (measure yourself)
+% Visual stimulus properties relating to monitor (measure yourself),
 % maxdotsframe is for RDK and is a limitation of your graphics card. The
-% only way you can know its limit is by trial and error.
+% only way you can know its limit is by trial and error. Variables monWidth
+% and viewDist are measured in centimeters.
 maxdotsframe = 150; monWidth = 50.8; viewDist = 120;
 
 % General drawing color used for RDK, instructions, etc.
@@ -59,22 +66,25 @@ block = 'RDKHoop_stairVis';
 filename = collect_subject_information(block);
 
 %% Initialize
-% curScreen = 0 if there is only one monitor. Check display settings on PC
-% if there is more than one monitor. curScreen will probably equal 1 or 2 
+% curScreen = 0 if there is only one monitor. If more than one monitor, 
+% check display settings on PC. curScreen will probably equal 1 or 2 
 % (e.g. monitor for stimulus presentation and monitor to run MATLAB code).
 curScreen=0;
 
 % Opens psychtoolbox and initializes experiment
-[screenInfo, curWindow, screenRect] = initialize_exp(monWidth,viewDist,curScreen);
+[screenInfo, curWindow, screenRect] = initialize_exp(monWidth, viewDist, curScreen);
 
 %% Welcome and Instrctions for the Suject
-% Opens psychtoolbox instructions for the specific task.
+% Opens psychtoolbox instructions for participant for the specific task 
+% (psyVis for all visual tasks, psyAud for all auditory only tasks, psyAV 
+% for all audiovisual tasks). trainAud and trainVis have separate instructions.
 instructions_psyVis(curWindow, cWhite0);
 
 %% Flip up fixation dot
 fix = fixation_dot_flip(screenRect,curWindow);
 
-% Initialize matrix to store data
+% Initialize matrix to store data. Data is recorded every trial using
+% function record_data
 data_output = zeros(num_trials, 6);
 
 % Generate the list of possible coherences by decreasing log values
@@ -90,15 +100,16 @@ visInfo = cohSet_generation(visInfo, nlog_coh_steps, nlog_division);
 visInfo.probs = [0.33 0.5 0.66 0.5];
 
 %% Experiment Loop
+% Loop through every trial.
 for ii= 1:num_trials
     
     if ii == 1 % the first trial in the staircase
         staircase_index = 1;
-        % Start staircase on random direction (l or r)
+        % Start staircase on random direction (left or right)
         visInfo.dir = randi([1,2]);
         % Start staircase on first coherence in cohSet
         visInfo.coh = visInfo.cohSet(staircase_index);
-    elseif ii > 1
+    elseif ii > 1 % every trial in staircase except for first trial
         % Function staircase_procedure takes the previous trial's accuracy
         % (incorr or corr) and uses a random number (0 to 1) to determine the
         % coherence and direction for the current trial. All based on
@@ -116,7 +127,7 @@ for ii= 1:num_trials
     end
 
     %% Create info matrix for Visual Stim. 
-    % This dotInfo informs at_dotGenerate how to generate the RDK. 
+    % This dotInfo  output informs at_dotGenerate how to generate the RDK. 
     dotInfo = at_createDotInfo(inputtype, visInfo.coh, visInfo.dir, typeInt, ...
         minNum, maxNum, meanNum, maxdotsframe, dur, block_dot_speed, vel_stair, visInfo.vel);
     
@@ -131,7 +142,7 @@ for ii= 1:num_trials
     %% Dot Generation.
     % This function plots the dots and updates them frame by frame in
     % accordance with their coherence, direction, and other dotInfo.
-    [resp,rt,start_time] = at_dotGenerate(visInfo, dotInfo, screenInfo, ...
+    [resp, rt, start_time] = at_dotGenerate(visInfo, dotInfo, screenInfo, ...
     screenRect, monWidth, viewDist, maxdotsframe, dur, curWindow, fix, ...
     responded, resp, rt);
     
