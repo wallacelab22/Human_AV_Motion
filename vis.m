170480a4265c9f7d15cd70574668062ee77e1c89
wallacelab22 <wallacelab.github@gmail.com>
2023-08-23 13:37:48 -0500

    AV slider implementation

--- vis.m
+++ vis.m
@@ -32,14 +32,14 @@ inputtype = 1; typeInt = 1; minNum = 1.5; maxNum = 2.5; meanNum = 2;
 % dur is stimulus duration, triallength is total length of 1 trial (this is
 % currently unused in code), Fs is sampling rate, nbblocks is used to divide up num_trials 
 % into equal parts to give subject breaks if there are many trials. 
-% Set to 0 if num_trials is shor  t   a nd subject does not need break(s).
-dur = 2; Fs = 44100; triallength = 2; nbblocks = 0; 
+% Set to 0 if num_trials is short and subject does not need break(s).
+dur = 0.5; Fs = 44100; triallength = 2; nbblocks = 0; 
 
 % Define buffersize in  order to make CAM (auditory stimulus)
 silence = 0.03; buffersize = (dur+silence)*Fs;
 
-% All variables that define stimulus repetitions; num_trials defines total
-% number of staircase trials, stimtrials defines number of stimulus trials
+% All variables that de 2fine stimulus repetitions; num_trials defines total
+% number of stair 2 2 1 1 1case trials, stimtrials defines number of stimulus trials
 % per condition for MCS, catchtrials defines total number of catch trials
 % for MCS.
 num_trials = 100; stimtrials = 12; catchtrials = 25;
@@ -100,19 +100,24 @@ textColor = cWhite0;
 % Opens psychtoolbox instructions for participant for the specific task 
 % (psyVis for all visual tasks, psyAud for all auditory only tasks, psyAV 
 % for all audiovisual tasks). trainAud and trainVis have separate instructions.
-%instructions_psyVis(curWindow, cWhit e0);
+%instructions_psyVis(curWindow, cWhite0);
 
 %% Flip up fixation dot
 [fix, s] = at_presentFix(screenRect, curWindow);
 
+audInfo.durRaw = dur;
 givenCoh = 0.5;
+block = 'AV';
+data_output = [1 0.5 1 1; 1 0.9 1 1; 1 0 1 1; 1 1 1 1; 1 0.3 1 1; 1 0.6 1 1; 1 0.2 1 1; 1 0.05 1 1; 1 0 1 1; 1 0.7 1 1; 1 0.4 1 1; 1 0.1 1 1; 1 0.15 1 1; 1 0.5 1 1; 1 0.3 1 1; 1 0.07 1 1];
+noise_reduction_scalar = 1;
+
 %% Experiment Loop
-% Loop through every trial.6
+% Loop through every trial.
 for ii = 1:length(data_output)
 
-    visInfo.dir = data_output(ii,3);
     audInfo.dir = data_output(ii,1);
     audInfo.coh = data_output(ii,2);
+    visInfo.dir = data_output(ii,3);
     givenCoh = data_output(ii,4);
     while true
         visInfo.coh = givenCoh;
@@ -136,96 +141,118 @@ for ii = 1:length(data_output)
         responded = 0; %DS mark as no response yet
         resp = nan; %default response is nan
         rt = nan; %default rt in case no response is recorded
-    
+        continue_show = round(dur*60);
+        priorityLevel = MaxPriority(curWindow,'KbCheck'); %make sure Window commands are correct
+        Priority(priorityLevel);
+
         % Create marker for EEG
-        [markers] = at_generateMarkers(data_output, ii, EEG_nature);
+        [markers] = at_generateMarkers(data_output, ii, EEG_nature, block);
+ 
+        %% Dot Generation.
+        % This function generates the dots that will be presented to
+        % participant in accordance with their coherence, direction, and other dotInfo.
+        [center, dotSize, d_ppd, ndots, dxdy, ss, Ls, continue_show] = at_generateDot(visInfo, dotInfo, screenInfo, screenRect, monWidth, viewDist, maxdotsframe, dur, curWindow, fix);
+        
+        %% Generate auditory stimulus
+        % Generates auditory stimulus
+        frames = 0;
+        % makeCAM and makeCAM_PILOT create an array voltages to be presented
+        % via speakers that creates perceptual auditory motion from one speaker
+        % to another
+        CAM = makeCAM_PILOT(audInfo.coh, audInfo.dir, audInfo.durRaw, silence, Fs, noise_reduction_scalar);
+        wavedata = CAM;
+        nrchannels = size(wavedata,1); % Number of rows = number of channels.
+
+
+        %% AV Stimuli Presentation
+        % This function uses psychtoolbox to present dots and auditory stimulus to participant.
+        [resp, rt, start_time] = at_presentAV(continue_show, responded, resp, rt, curWindow, fix, frames, pahandle, wavedata, visInfo, dotInfo, center, dotSize, ...
+            d_ppd, ndots, dxdy, ss, Ls, EEG_nature, outlet, markers);
+
+        %% Erase last dots & go back to only plotting fixation
+        Screen('DrawingFinished',curWindow);
+        Screen('DrawDots', curWindow, [0; 0], 10, fixation_color, fix, 1);
+        Screen('Flip', curWindow, 0);
+        
+        %% ITI & response recording
+        [resp, rt] = iti_response_recording(typeInt, minNum, maxNum, meanNum, dur, start_time, keyisdown, responded, resp, rt);
+        
+        %% Direction conversion
+        visInfo = direction_conversion(visInfo);
+     
+        WaitSecs(0.5)
     
         while true
-            %% Dot Generation.
-            % This function generates the dots that will be presented to
-            % participant in accordance with their coherence, direction, and other dotInfo.
-            [center, dotSize, d_ppd, ndots, dxdy, ss, Ls, continue_show] = at_generateDot(visInfo, dotInfo, screenInfo, screenRect, monWidth, viewDist, maxdotsframe, dur, curWindow, fix);
-            
-            %% Dot Presentation
-            % This function uses psychtoolbox to present dots to participant.
-            [resp, rt, start_time] = at_presentDot(visInfo, dotInfo, center, dotSize, d_ppd, ndots, dxdy, ss, Ls, continue_show, curWindow, fix, responded, resp, rt, EEG_nature, outlet, markers);
-        
-            %% Erase last dots & go back to only plotting fixation
-            Screen('DrawingFinished',curWindow);
-            Screen('DrawDots', curWindow, [0; 0], 10, fixation_color, fix, 1);
-            Screen('Flip', curWindow, 0);
-            
-            %% ITI & response recording
-            [resp, rt] = iti_response_recording(typeInt, minNum, maxNum, meanNum, dur, start_time, keyisdown, responded, resp, rt);
-            
-            %% 2 Direction conversion
-            visInfo = direction_conversion(visInfo);
-        
-            %% Save data into data_output on a trial by trial basis
-            [trial_status, data_output] = record_data(data_output, right_var, left_var, right_keypress, left_keypress, visInfo, resp, rt, ii, vel_stair);
-        
-            WaitSecs(0.5)
-        
-            while true
-                [keyisDown, secs, keyCode] = KbCheck;
-                pressedKeys = find(keyCode);
-                if ismember(pressedKeys, right_keypress)
-                    LineX = LineX + pixelsPerPress;
-                elseif ismember(pressedKeys, left_keypress)
-                    LineX = LineX - pixelsPerPress;
-                elseif ismember(pressedKeys, space_keypress)
-                    StopPixel_M = ((LineX - xCenter) + halfLength)/divider; % for a rating of between 0 and 10. Tweak this as necessary
-                    break
-                end
-                if LineX < (xCenter - halfLength)
-                    LineX = xCenter - halfLength;
-                elseif LineX > (xCenter + halfLength)
-                    LineX = xCenter + halfLength;
-                end
-                if LineY < 0
-                    LineY = 0;
-                elseif LineY > (yCenter + 10)
-                    LineY = yCenter + 10;
-                end
-            
-                centeredRect = CenterRectOnPointd(baseRect, LineX, LineY);
-            
-                currentRating = ((LineX - xCenter) + halfLength)/divider;
-                ratingText = num2str(currentRating); % to make this display whole numbers, use "round(currentRating)"
-                ratingText = sprintf(ratingText, '%');
-            
-                DrawFormattedText(curWindow, ratingText,'center', (yCenter-200), textColor, [],[],[],5); % display current rating 
-                DrawFormattedText(curWindow, question ,'center', (yCenter-100), textColor, [],[],[],5);
-                
-                Screen('DrawLine', curWindow,  lineColor, (xCenter+halfLength), (yCenter),(xCenter-halfLength), (yCenter), 1);
-                Screen('DrawLine', curWindow,  lineColor, (xCenter+halfLength), (yCenter+10), (xCenter+halfLength), (yCenter-10), 1);
-                Screen('DrawLine', curWindow,  lineColor, (xCenter-halfLength), (yCenter+10), (xCenter-halfLength), (yCenter-10), 1);
-                
-                Screen('DrawText', curWindow, lowerText, (xCenter-halfLength), (yCenter+25),  textColor);
-                Screen('DrawText', curWindow, upperText , (xCenter+halfLength) , (yCenter+25), textColor);
-                Screen('FillRect', curWindow, rectColor, centeredRect);
-                vbl = Screen('Flip', curWindow, vbl + (waitframes - 0.5) *  slack);
+            [keyisDown, secs, keyCode] = KbCheck;
+            pressedKeys = find(keyCode);
+            if ismember(pressedKeys, right_keypress)
+                LineX = LineX + pixelsPerPress;
+            elseif ismember(pressedKeys, left_keypress)
+                LineX = LineX - pixelsPerPress;
+            elseif ismember(pressedKeys, space_keypress)
+                StopPixel_M = ((LineX - xCenter) + halfLength)/divider; % for a rating of between 0 and 10. Tweak this as necessary
+                break
             end
-            Screen('DrawText', curWindow, 'Do you think you have matched the visual noisiness to the auditory noisiness?',300,300,cWhite0);
-            Screen('DrawText', curWindow, 'If you think you are complete with this trial, press 1',300,400,cWhite0);
-            Screen('DrawText', curWindow, 'If you want to see/hear your adjustments, press 2',300,500,cWhite0);
-            Screen('Flip',curWindow);
+            if LineX < (xCenter - halfLength)
+                LineX = xCenter - halfLength;
+            elseif LineX > (xCenter + halfLength)
+                LineX = xCenter + halfLength;
+            end
+            if LineY < 0
+                LineY = 0;
+            elseif LineY > (yCenter + 10)
+                LineY = yCenter + 10;
+            end
+        
+            centeredRect = CenterRectOnPointd(baseRect, LineX, LineY);
+        
+            currentRating = ((LineX - xCenter) + halfLength)/divider;
+            ratingText = num2str(currentRating); % to make this display whole numbers, use "round(currentRating)"
+            ratingText = sprintf(ratingText, '%');
+        
+            DrawFormattedText(curWindow, ratingText,'center', (yCenter-200), textColor, [],[],[],5); % display current rating 
+            DrawFormattedText(curWindow, question ,'center', (yCenter-100), textColor, [],[],[],5);
+            
+            Screen('DrawLine', curWindow,  lineColor, (xCenter+halfLength), (yCenter),(xCenter-halfLength), (yCenter), 1);
+            Screen('DrawLine', curWindow,  lineColor, (xCenter+halfLength), (yCenter+10), (xCenter+halfLength), (yCenter-10), 1);
+            Screen('DrawLine', curWindow,  lineColor, (xCenter-halfLength), (yCenter+10), (xCenter-halfLength), (yCenter-10), 1);
+            
+            Screen('DrawText', curWindow, lowerText, (xCenter-halfLength), (yCenter+25),  textColor);
+            Screen('DrawText', curWindow, upperText , (xCenter+halfLength) , (yCenter+25), textColor);
+            Screen('FillRect', curWindow, rectColor, centeredRect);
+            vbl = Screen('Flip', curWindow, vbl + (waitframes - 0.5) *  slack);
+        end
+        Screen('DrawText', curWindow, 'Do you think you have matched the visual noisiness to the auditory noisiness?',300,300,cWhite0);
+        Screen('DrawText', curWindow, 'If you think you are complete with this trial, press 1',300,400,cWhite0);
+        Screen('DrawText', curWindow, 'If you want to see/hear your adjustments, press 2',300,500,cWhite0);
+        Screen('Flip',curWindow);
+        while true
             [keyisDown, secs, keyCode] = KbCheck;
             pressedKeys = find(keyCode);
             if ismember(pressedKeys, right_keypress)
                 break;
-            elseif ismember(pressedKeys, left_keypress)
-                continue;
+                done_status = 1;
+            elseif ismember(presssedKeys, left_keypress)
+                break;
+                done_status = 0;
             else
-                WaitSecs(3)
+                continue;
             end
         end
-    end
-    WaitSecs(0.5)
-    
-    givenCoh = StopPixel_M/100;
 
- end
+        if done_status == 1
+            break;
+        end
+
+        WaitSecs(0.5)
+
+        % Update visual coherence with the givenCoh
+        givenCoh = StopPixel_M/100;
+    end
+    
+    [trial_status, data_output] = record_data(data_output, right_var, left_var, right_keypress, left_keypress, visInfo, resp, rt, ii, vel_stair);
+ 
+end
 
 %% Goodbye
 cont(curWindow, cWhite0);
