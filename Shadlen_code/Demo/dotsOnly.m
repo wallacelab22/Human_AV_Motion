280e363c42e1c4880a4a9f353c6e276a1c7d8e0e
Adam Tiesman <adam.j.tiesman@vanderbilt.edu>
2024-10-11 23:06:23 -0500

    removing old code and unnecessary files from repo

--- Shadlen_code/Demo/dotsOnly.m
+++
@@ -1,6 +0,0 @@
-function [dot_info, frames, rseed, start_time, end_time, response, response_time] = RDK_draw(inputtype)
-% This function initializes the RDK on a trial by trial basis
-
-dot_info = createDotInfo(1);
-[frames, rseed, start_time, end_time, response, response_time] = dotsX(screenInfo, dotInfo);    
-pause(0.5)
